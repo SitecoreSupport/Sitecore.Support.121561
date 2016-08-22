4da7072586075fced1232507bbb95a5690b00a55
AlenPelin <AlenPelin@gmail.com>
2016-08-22 14:11:36 +1000

    Add UtcNow property

--- src/Sitecore.Support.121561/Analytics/Data/MongoDb/ProcessingPool/MongoDbProcessingPool.cs
+++ src/Sitecore.Support.121561/Analytics/Data/MongoDb/ProcessingPool/MongoDbProcessingPool.cs
@@ -7,6 +7,9 @@
 
   public class MongoDbProcessingPool : Sitecore.Analytics.Data.MongoDb.ProcessingPool.MongoDbProcessingPool
   {
+    const string StackTrace = "StackTrace";
+    const string UtcNow = "UtcNow";    
+
     public MongoDbProcessingPool(MongoDbCollection pool) : base(pool)
     {
     }
@@ -21,12 +24,31 @@
     {
       if (Debug)
       {
-        // remove unncessary lines from stacktrace
-        var lines = Environment.StackTrace.Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-        var stackTrace = string.Join("\r\n", lines.Skip(2).TakeWhile(line => !line.Contains("System.Threading.ExecutionContext.Run")));
+        // remove unncessary lines from stacktrace, skipping first 2 Environment.StackTrace calls and last System.Threading.ExecutionContext.Run
+        var lines = Environment.StackTrace.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+        var stackTrace = string.Join(Environment.NewLine, lines.Skip(2).TakeWhile(line => !line.Contains("System.Threading.ExecutionContext.Run")));
 
         // add normalized stacktrace to properties
-        workItem.Properties.Add("StackTrace", stackTrace);
+        var properties = workItem.Properties;
+        if (properties.ContainsKey(StackTrace))
+        {
+          properties[StackTrace] = stackTrace;
+        }
+        else
+        {
+          properties.Add(StackTrace, stackTrace);
+        }
+
+        // add UTC timestamp                      
+        var utcNow = DateTime.UtcNow.ToString();
+        if (properties.ContainsKey(UtcNow))
+        {
+          properties[UtcNow] = utcNow;
+        }
+        else
+        {
+          properties.Add(UtcNow, utcNow);
+        }
       }
 
       return base.Add(workItem, options);
